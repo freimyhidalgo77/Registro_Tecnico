3929893d803b70b80b69cb677c1a2d18c6a95432
fahh7 <fahh7@FREIMYHP>
2024-09-06 22:56:23 -0400

    Añadiendo nevos atributos de tipos tecnicos a el Modelo Tecnicos.

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -1,23 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RegistroTecnicos.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 
 namespace RegistroTecnicos.Models
 {
-	public class Tecnicos
-	{
-		[Key]
-		//Atributos Tecnico (Id, Nombre, Sueldo por hora)
-		public int TecnicoId { get; set; }
+    public class Tecnicos
+    {
+        [Key]
+        //Atributos Tecnico (Id, Nombre, Sueldo por hora)
+        public int TecnicoId { get; set; }
 
-		[Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
 
-		[RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
-		public string NombreTecnico { get; set; }
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
+        public string NombreTecnico { get; set; }
 
 
-		[Required (ErrorMessage = "Campo sueldo por hora obligatrio ")]   
+        [Required(ErrorMessage = "Campo sueldo por hora obligatrio ")]
 
-		public decimal SueldoHora {get; set;}    
-		     
-	}   
-}  
-   
+        public decimal SueldoHora { get; set; }
+
+        [ForeignKey("TipoTecnicos")]
+        [Required(ErrorMessage = "Seleccione un tipo de tecnico ")]
+
+        public int TipoId { get; set; }
+
+        [ForeignKey("TipoId")]
+        public TiposTecnicos TipoTecnicos { get; set; }
+
+
+
+    }
+} 
