3fe27a20959aa4db2f5faccf4b0906920af85ecc
fahh7 <fahh7@FREIMYHP>
2024-09-06 22:48:36 -0400

    Añadiendo  el modelo TipoTecnicos al contexto.

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
+
 namespace RegistroTecnicos.DAL
 {
-	public class Context : DbContext
-	{
-		//Conexion con la base de datos (El contexto o context)
-		public Context(DbContextOptions<Context> options) : base(options)
-		{
+    public class Context : DbContext
+    {
+        //Conexion con la base de datos (El contexto o context)
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
 
-		}
+        }
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+        public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
 
-		public DbSet<Tecnicos> Tecnicos {get; set;}
-		 
-	}
+    }
 
 
 }
 
+ 
