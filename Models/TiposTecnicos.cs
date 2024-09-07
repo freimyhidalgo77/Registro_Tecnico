26df2efa0085dc4939e112f772da54bc87988759
fahh7 <fahh7@FREIMYHP>
2024-09-06 22:45:26 -0400

    Añadiendo el modelo TipoTecnicos.

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+
+    public class TiposTecnicos
+    {
+        [Key]
+        public int TipoId { get; set; }
+
+        [Required(ErrorMessage = "Favor de agregar una descripcion")]
+
+        public string Descripcion { get; set; }
+
+
+
+
+    }
+}
+            
