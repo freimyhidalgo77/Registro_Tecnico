7c16c4fd7c96dd25cd4a8694106699ee75a6adc0
fahh7 <fahh7@FREIMYHP>
2024-09-06 22:44:44 -0400

    Inyectando service de TecnicoService y TipoTecnicoservice,

--- Program.cs
+++ Program.cs
@@ -1,48 +1,48 @@
-using RegistroTecnicos.Components;
-using RegistroTecnicos.DAL;
-using RegistroTecnicos.Models;
 using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Components;
+using RegistroTecnicos.Models;
+using RegistroTecnicos.DAL;
 using RegistroTecnicos.Service;
 
 namespace RegistroTecnicos
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
-			//Inyeccion de la base de datos (SqLite)
-			var ConStr = builder.Configuration.GetConnectionString("ConStr");
-			builder.Services.AddDbContext<Context>(c => c.UseSqlite(ConStr));
+            //Inyeccion de la base de datos (SqLite)
+            var ConStr = builder.Configuration.GetConnectionString("ConStr");
+            builder.Services.AddDbContext<Context>(c => c.UseSqlite(ConStr));
 
-			//Inyeccion del servicio (service)
-			builder.Services.AddScoped<TecnicoService>();
-		
-			// Add services to the container.
-			builder.Services.AddRazorComponents()
-				.AddInteractiveServerComponents();
+            //Inyeccion del servicio (service)
+            builder.Services.AddScoped<TecnicoService>();
+            builder.Services.AddScoped<TiposTecnicoService>();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-			app.UseStaticFiles();
-			app.UseAntiforgery();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
 
-			app.MapRazorComponents<App>()
-				.AddInteractiveServerRenderMode();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }   
+    }
 }
