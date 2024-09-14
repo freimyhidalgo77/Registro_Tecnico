8129f85956ce2efb13f83c047820c6750cf44c83
fahh7 <fahh7@FREIMYHP>
2024-09-13 21:19:58 -0400

    Modelo cliente.

---
+++ Models/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+	public class Clientes
+	{
+
+		[Key]
+
+		public int ClienteId { get; set; }
+
+		[Required(ErrorMessage = "Campo nombre obligatorio")]
+
+		[RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
+		public string NombreCliente { get; set; }
+
+
+		[Required(ErrorMessage = "Campo numero de whatsapp obligatorio")]
+
+		public string NumeroWhatsapp { get; set; }
+
+
+
+
+		  
+	}
+}
