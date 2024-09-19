fc2f1788be6cb03d7d44b093654e34bf25eb09b9
fahh7 <fahh7@FREIMYHP>
2024-09-19 08:01:25 -0400

    Creando el modelo prioridades.

---
+++ Models/Prioridades.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RegistroTecnicos.Models
+{
+    public class Prioridades
+    {
+
+        [Key]
+
+        public int PrioridadId { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Tiempo { get; set; }
+
+
+    }
+}
+           
