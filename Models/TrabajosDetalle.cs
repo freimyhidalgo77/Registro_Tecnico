5463b9561265dec2d1ed6ca6b7897804fef19eee
fahh7 <fahh7@FREIMYHP>
2024-10-20 13:16:15 -0400

    Creando modelo TrabajosDetalle

---
+++ Models/TrabajosDetalle.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class TrabajosDetalle
+    {
+
+            [Key]
+
+            public int DetalleId { get; set; }
+
+            [ForeignKey("TrabajoId")]
+            public int TrabajoId { get; set; }
+            public Trabajos? Trabajos { get; set; }
+
+            [ForeignKey("ArticuloId")]
+            public int ArticuloId { get; set; }
+            public Articulos? Articulo { get; set; }
+
+            [Required(ErrorMessage = "Ingrese una cantidad")]
+            public int cantidad { get; set; }
+
+            [Required(ErrorMessage = "Ingrese un precio")]
+            public decimal Precio { get; set; }
+
+            [Required(ErrorMessage = "Ingrese un costo")]
+            public decimal Costo { get; set; }
+
+
+        }
+}
