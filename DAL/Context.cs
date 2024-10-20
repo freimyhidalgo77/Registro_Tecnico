224e7b1d1cdc9e0b2a337d04cbc36a90e8e30634
fahh7 <fahh7@FREIMYHP>
2024-10-20 18:59:29 -0400

    cambiando los precios.

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -5,13 +5,12 @@ using RegistroTecnicos.Models;
 namespace RegistroTecnicos.DAL
 {
 
-
     public class Context : DbContext
     {
         //Conexion con la base de datos (El contexto o context)
         public Context(DbContextOptions<Context> options) : base(options)
         {
-
+        
         }
         public DbSet<Tecnicos> Tecnicos { get; set; }
         public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
@@ -21,7 +20,7 @@ namespace RegistroTecnicos.DAL
         public DbSet<Articulos> Articulos { get; set; }
         public DbSet<TrabajosDetalle> TrabajosDetalle { get; set; }
 
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -30,7 +29,7 @@ namespace RegistroTecnicos.DAL
             new Articulos(){ArticuloId=1, descripcion= "Tarjeta Grafica RTX 4060 ", costo= 100, precio= 165, existencia= 50},
             new Articulos(){ArticuloId=2, descripcion="Teclado Gaming K300 QWERY", costo= 225, precio= 275, existencia= 50},
             new Articulos(){ArticuloId=3, descripcion="Mouse Gamer  Logitech G502", costo= 25, precio= 35, existencia= 50},
-            new Articulos(){ArticuloId=4, descripcion="Monitor Sony Inzone M9", costo= 30, precio= 38, existencia= 50 }
+            new Articulos(){ArticuloId=4, descripcion="Monitor Sony Inzone M9", costo= 30, precio= 38, existencia= 50}
         });
         }
 
