9ae99ebac291609ce4c6a80a6b1437e4ea4336b2
fahh7 <fahh7@FREIMYHP>
2024-10-20 13:14:23 -0400

    Agregando modelo Articulos.

---
+++ Models/Articulos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Articulos
+    {
+        [Key]
+
+        public int ArticuloId { get; set; }
+
+        [Required(ErrorMessage = "Favor llenar el campo descripcion")]
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "Favor llenar el campo costo")]
+        public decimal costo { get; set; }
+
+        [Required(ErrorMessage = "Favor llenar el campo precio")]
+        public decimal precio { get; set; }
+
+        [Required(ErrorMessage = "Favor llenar el campo existecia")]
+        public int existencia { get; set; } 
+
+    }
+}       
