4f02b877693f6bee930ba7cb4a63737e044943b1
fahh7 <fahh7@FREIMYHP>
2024-10-20 13:15:32 -0400

    Creando modelo TrabajosDetalle

---
+++ Service/TrabajosDetalleService.cs
@@ -0,0 +1,26 @@
+﻿using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroTecnicos.Service
+{
+    public class TrabajosDetalleService
+    {
+
+        private readonly Context _contexto;
+
+        public TrabajosDetalleService(Context context)
+        {
+            _contexto = context;
+        }
+
+
+        public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+        {
+            return await _contexto.Articulos.AsNoTracking().Where(criterio).ToListAsync();
+        }
+
+
+    }
+}
