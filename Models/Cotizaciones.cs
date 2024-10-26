7c4d8643c757bd23e006f93cdecc4cc5bd849b6a
fahh7 <fahh7@FREIMYHP>
2024-10-26 15:05:07 -0400

    Añadiendo modelo de Cotizaciones

---
+++ Models/Cotizaciones.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Cotizaciones
+    {
+
+        [Key]
+
+        public int CotizacionId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir una fecha")]
+        public DateTime Fecha { get; set; }
+
+
+        [ForeignKey("Clientes")]
+        public int ClienteId { get; set; }
+        public Clientes? Clientes { get; set; }
+
+
+        [Required(ErrorMessage = "Es obligatorio introducir una observacion")]
+        public string? Observacion { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir un monto")]
+
+        public decimal Monto { get; set; }
+
+        public ICollection<CotizacionesDetalles> CotizacionesDetalles { get; set; } = new List<CotizacionesDetalles>();
+
+
+
+    }
+}
