886af39ac64976594944b4497bb12b7f37cf8c86
fahh7 <fahh7@FREIMYHP>
2024-10-26 15:03:54 -0400

    Añadiendo el contextFactory al servicio de cotizacion detalle

---
+++ Service/CotizacionesDetalleService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Service
+{
+    public class CotizacionesDetalleService(IDbContextFactory<Context> DbFactory)
+    {
+        private readonly Context _context;
+
+		public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+		{
+			await using var context = await DbFactory.CreateDbContextAsync();
+			return await context.Articulos.Where(criterio).ToListAsync();
+		}
+
+	}
+}
