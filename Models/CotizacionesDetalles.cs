34ad4cf37e73de77965df2d1cf586ad324cdb79a
fahh7 <fahh7@FREIMYHP>
2024-10-26 15:05:30 -0400

    Añadiendo modelo de CotizacionesDetalles

---
+++ Models/CotizacionesDetalles.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class CotizacionesDetalles
+    {
+
+        [Key]
+        public int DetalleId { get; set; }
+
+        [ForeignKey("CotizacionId")]
+        public int CotizacionesId { get; set; }
+        public Cotizaciones? cotizaciones { get; set; }
+
+
+        [ForeignKey("ArticuloId")]
+        public int ArticuloId { get; set; }
+        public Articulos? Articulos { get; set; }
+
+
+        [Required(ErrorMessage = "obligatorio introducir una cantidad")]
+        public int cantidad { get; set; }
+
+        [Required(ErrorMessage = "obligatorio introducir un precio")]
+        public decimal precio { get; set; }
+
+
+    }
+}
