a11937cdcb0b7c3757559bd251ee85ad3f01dbf6
fahh7 <fahh7@FREIMYHP>
2024-10-26 21:51:05 -0400

    Corrigiendo modelo

--- Models/Cotizaciones.cs
+++ Models/Cotizaciones.cs
@@ -11,21 +11,23 @@ namespace RegistroTecnicos.Models
         public int CotizacionId { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir una fecha")]
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
 
-        [ForeignKey("Clientes")]
+        [ForeignKey("ClienteId")]
         public int ClienteId { get; set; }
         public Clientes? Clientes { get; set; }
 
-
         [Required(ErrorMessage = "Es obligatorio introducir una observacion")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+
         public string? Observacion { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir un monto")]
 
         public decimal Monto { get; set; }
 
+        [ForeignKey("CotizacionId")]
         public ICollection<CotizacionesDetalles> CotizacionesDetalles { get; set; } = new List<CotizacionesDetalles>();
 
 
