50bf144c2e9e22a50a2e59b2eccc203112afee02
fahh7 <fahh7@FREIMYHP>
2024-10-26 23:26:55 -0400

    Migracion lista

---
+++ Migrations/20241027013938_Inicial.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    costo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    existencia = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroWhatsapp = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prioridades",
+                columns: table => new
+                {
+                    PrioridadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tiempo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prioridades", x => x.PrioridadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposTecnicos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposTecnicos", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cotizaciones",
+                columns: table => new
+                {
+                    CotizacionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    ClientesClienteId = table.Column<int>(type: "int", nullable: true),
+                    Observacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Monto = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cotizaciones", x => x.CotizacionId);
+                    table.ForeignKey(
+                        name: "FK_Cotizaciones_Clientes_ClientesClienteId",
+                        column: x => x.ClientesClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreTecnico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SueldoHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TipoId = table.Column<int>(type: "int", nullable: false),
+                    TipoTecnicosTipoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicoId);
+                    table.ForeignKey(
+                        name: "FK_Tecnicos_TiposTecnicos_TipoTecnicosTipoId",
+                        column: x => x.TipoTecnicosTipoId,
+                        principalTable: "TiposTecnicos",
+                        principalColumn: "TipoId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CotizacionesDetalles",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CotizacionId = table.Column<int>(type: "int", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false),
+                    cantidad = table.Column<int>(type: "int", nullable: false),
+                    precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CotizacionesDetalles", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_CotizacionesDetalles_Articulos_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulos",
+                        principalColumn: "ArticuloId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CotizacionesDetalles_Cotizaciones_CotizacionId",
+                        column: x => x.CotizacionId,
+                        principalTable: "Cotizaciones",
+                        principalColumn: "CotizacionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trabajos",
+                columns: table => new
+                {
+                    TrabajoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Monto = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    TecnicoId = table.Column<int>(type: "int", nullable: false),
+                    TipoId = table.Column<int>(type: "int", nullable: false),
+                    PrioridadId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trabajos", x => x.TrabajoId);
+                    table.ForeignKey(
+                        name: "FK_Trabajos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Trabajos_Prioridades_PrioridadId",
+                        column: x => x.PrioridadId,
+                        principalTable: "Prioridades",
+                        principalColumn: "PrioridadId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Trabajos_Tecnicos_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "TecnicoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Trabajos_TiposTecnicos_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "TiposTecnicos",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrabajosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrabajoId = table.Column<int>(type: "int", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false),
+                    cantidad = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Costo = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrabajosDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_TrabajosDetalle_Articulos_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulos",
+                        principalColumn: "ArticuloId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrabajosDetalle_Trabajos_TrabajoId",
+                        column: x => x.TrabajoId,
+                        principalTable: "Trabajos",
+                        principalColumn: "TrabajoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Articulos",
+                columns: new[] { "ArticuloId", "costo", "descripcion", "existencia", "precio" },
+                values: new object[,]
+                {
+                    { 1, 100m, "Tarjeta Grafica RTX 4060 ", 50, 165m },
+                    { 2, 225m, "Teclado Gaming K300 QWERY", 50, 275m },
+                    { 3, 25m, "Mouse Gamer  Logitech G502", 50, 35m },
+                    { 4, 30m, "Monitor Sony Inzone M9", 50, 38m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cotizaciones_ClientesClienteId",
+                table: "Cotizaciones",
+                column: "ClientesClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CotizacionesDetalles_ArticuloId",
+                table: "CotizacionesDetalles",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CotizacionesDetalles_CotizacionId",
+                table: "CotizacionesDetalles",
+                column: "CotizacionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tecnicos_TipoTecnicosTipoId",
+                table: "Tecnicos",
+                column: "TipoTecnicosTipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trabajos_ClienteId",
+                table: "Trabajos",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trabajos_PrioridadId",
+                table: "Trabajos",
+                column: "PrioridadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trabajos_TecnicoId",
+                table: "Trabajos",
+                column: "TecnicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trabajos_TipoId",
+                table: "Trabajos",
+                column: "TipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrabajosDetalle_ArticuloId",
+                table: "TrabajosDetalle",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrabajosDetalle_TrabajoId",
+                table: "TrabajosDetalle",
+                column: "TrabajoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CotizacionesDetalles");
+
+            migrationBuilder.DropTable(
+                name: "TrabajosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Cotizaciones");
+
+            migrationBuilder.DropTable(
+                name: "Articulos");
+
+            migrationBuilder.DropTable(
+                name: "Trabajos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Prioridades");
+
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+
+            migrationBuilder.DropTable(
+                name: "TiposTecnicos");
+        }
+    }
+}
